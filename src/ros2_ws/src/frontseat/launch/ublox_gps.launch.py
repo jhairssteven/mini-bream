de8dbd4f69d3f7006bd489d45a08f68a4e526bb9
Jhair Steven Gallego Mendez <71862429+jhairssteven@users.noreply.github.com>
2025-08-27 18:02:37 +0000

    - launch gps node configuration directly from frontseat

--- src/ros2_ws/src/frontseat/launch/ublox_gps.launch.py
+++ src/ros2_ws/src/frontseat/launch/ublox_gps.launch.py
@@ -1,16 +1,26 @@
-import os
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import RegisterEventHandler, EmitEvent
+from launch.events import Shutdown
+import launch
+import os
 
 def generate_launch_description():
-    # Path to the other launch file
-    ublox_launch_file = os.path.join(
+
+    config_directory = os.path.join(
         get_package_share_directory('ublox_gps'),
-        'launch',
-        'ublox_gps_node-launch.py'
+        'config')
+    params = os.path.join(config_directory, 'c94_m8p_rover.yaml')
+
+    ublox_gps_node = Node(
+        package='ublox_gps',
+        executable='ublox_gps_node',
+        output='both',
+        parameters=[
+            params,      
+            {'rate': 19.0}
+        ]
     )
     
     imu_estimation = Node(
@@ -18,15 +28,19 @@ def generate_launch_description():
         executable='imu_estimation',
         name='imu_estimation',
         output='screen',
-        remappings=[
-            ('/wamv/sensors/gps/gps/fix', '/ublox_gps_node/fix')
-        ]
+
         )
 
     return LaunchDescription([
-        imu_estimation,
-        
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(ublox_launch_file)
-        )]
+        ublox_gps_node,
+        imu_estimation,        
+
+        # Event handler to shut down the whole launch file when the gps node dies
+        RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=ublox_gps_node,
+                on_exit=[EmitEvent(
+                    event=Shutdown())],
+            )),
+        ]
     )
