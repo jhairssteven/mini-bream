17cc7ad9a887c02a762d85d144ed474517075ea0
Jhair Steven Gallego Mendez <71862429+jhairssteven@users.noreply.github.com>
2025-09-25 01:48:24 +0000

    Create config dir to store camera params

--- src/ros2_ws/src/frontseat/launch/cameras.launch.py
+++ src/ros2_ws/src/frontseat/launch/cameras.launch.py
@@ -5,14 +5,14 @@ import os
 
 # Install dependency with sudo apt-get install ros-<ros2-distro>-usb-cam
 front_camera_params = os.path.join(
-    get_package_share_directory('frontseat'), 'fronseat', 'cameras', 'params.yaml'
+    get_package_share_directory('frontseat'), 'config', 'cameras', 'front_camera_params.yaml'
     )
 
 def generate_launch_description():
     web_cam_node = Node(
         package='usb_cam',
         executable='usb_cam_node_exe',
-        name='front_camera',
+        name='front_camera_node',
         output='screen',
         parameters=[front_camera_params],
 
