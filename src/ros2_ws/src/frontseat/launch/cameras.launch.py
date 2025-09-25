2ef6346a8938f8d010606088d9bbf4a0fefaa408
Jhair Steven Gallego Mendez <71862429+jhairssteven@users.noreply.github.com>
2025-09-25 01:40:15 +0000

    Add camera launch file

---
+++ src/ros2_ws/src/frontseat/launch/cameras.launch.py
@@ -0,0 +1,22 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+
+# Install dependency with sudo apt-get install ros-<ros2-distro>-usb-cam
+front_camera_params = os.path.join(
+    get_package_share_directory('frontseat'), 'fronseat', 'cameras', 'params.yaml'
+    )
+
+def generate_launch_description():
+    web_cam_node = Node(
+        package='usb_cam',
+        executable='usb_cam_node_exe',
+        name='front_camera',
+        output='screen',
+        parameters=[front_camera_params],
+
+    )
+    return LaunchDescription([
+        web_cam_node
+        ])
