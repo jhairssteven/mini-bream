783706084dd3afca8bf9f12b454805854a40c076
Jhair Steven Gallego Mendez <71862429+jhairssteven@users.noreply.github.com>
2025-10-13 20:59:28 +0000

    Add ublox_gps frontseat configurations on install

--- src/ros2_ws/src/frontseat/setup.py
+++ src/ros2_ws/src/frontseat/setup.py
@@ -12,7 +12,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', glob('launch/*')),
-        ('share/' + package_name + '/config/cameras', glob('config/cameras/*'))
+        ('share/' + package_name + '/config/cameras', glob('config/cameras/*')),
+        ('share/' + package_name + '/config/ublox_gps', glob('config/ublox_gps/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
