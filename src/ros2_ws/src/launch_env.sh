4446e7cf32c26424aec1c8f7e6decc69fc9f52fa
Jhair Steven Gallego Mendez <71862429+jhairssteven@users.noreply.github.com>
2025-11-26 18:09:39 +0000

    - launch tmux env using script

---
+++ src/ros2_ws/src/launch_env.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Define the sessions
+sessions="sim rviz other debug dubins"
+
+for session in $sessions; do
+    # Create a new detached session
+    tmux new-session -d -s "$session"
+
+    # Configure the session based on its name
+    case "$session" in
+        sim)
+            tmux send-keys -t "$session" "ros2 launch linc_gz docking_harner.launch.py" C-m
+            ;;
+        other)
+            # First command in the initial window
+            tmux send-keys -t "$session" "ros2 run frontseat twist_to_diff_drive" C-m
+            
+            # Second command in a new vertical split
+            tmux split-window -v -t "$session"
+            tmux send-keys -t "$session" "ros2 run frontseat gps_center_offset_node" C-m
+            
+            # Third command in another vertical split
+            tmux split-window -v -t "$session"
+            tmux send-keys -t "$session" "ros2 launch mission_planner action_client.launch.py" C-m
+            
+            # Adjust layout to be even vertical
+            tmux select-layout -t "$session" even-vertical
+            ;;
+        rviz)
+            # First command
+            tmux send-keys -t "$session" "rviz2" C-m
+            
+            # Second command in a new vertical split
+            tmux split-window -v -t "$session"
+            tmux send-keys -t "$session" "ros2 launch visualization_tools visuals.launch.py" C-m
+            
+            # Adjust layout
+            tmux select-layout -t "$session" even-vertical
+            ;;
+        dubins)
+            tmux send-keys -t "$session" "ros2 launch backseat mission.launch.py" C-m
+            ;;
+        debug)
+            # No specific commands for debug session
+            ;;
+    esac
+done
